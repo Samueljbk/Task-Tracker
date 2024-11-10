1b430603ce608c7dd9e7182476c066278d2a19aa
Samueljbk <samjbkyle@gmail.com>
2024-11-11 02:54:31 +1300

    Added add_task function

--- src/task_manager.py
+++ src/task_manager.py
@@ -9,6 +9,25 @@ def load_tasks():
     with open('tasks.json', 'r') as file:
         return json.load(file)
 
+# Save new tasks to tasks.json
 def save_tasks(tasks):
     with open('tasks.json', 'w') as file:
         json.dump(tasks, file, indent=4)
+
+# Add new task to tasks.json
+def add_task(description):
+    tasks = load_tasks() # Current list of tasks
+
+    # Creating a new task
+    unique_id = len(tasks) + 1 
+    new_task = {
+        "id": unique_id,
+        "description": description,
+        "status": "todo",
+        "createdAt": datetime.now().isoformat(),
+        "updatedAt": datetime.now().isoformat()
+    }
+
+    tasks.append(new_task)
+    save_tasks(tasks)
+    print(f"Output: Task added successfully (ID: {unique_id})")
